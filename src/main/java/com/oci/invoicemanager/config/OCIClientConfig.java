7a3326915f87670e531ef6404b06c20499985cd2
Vlad Novikov <uladzislau.novikau@matillion.com>
2023-12-13 11:06:26 +0100

    feat(notification): test commit

---
+++ src/main/java/com/oci/invoicemanager/config/OCIClientConfig.java
@@ -0,0 +1,45 @@
+package com.oci.invoicemanager.config;
+
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.AuthenticationDetailsProvider;
+import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
+import com.oracle.bmc.auth.StringPrivateKeySupplier;
+import com.oracle.bmc.ons.NotificationDataPlaneClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+import java.util.function.Supplier;
+
+@Configuration
+public class OCIClientConfig {
+
+  @ConfigurationProperties(prefix = "oci.client")
+  public record OCIClientProps(String tenancyId, String userId, String fingerprint, String privateKey, Region region) { }
+  @Bean
+  public AuthenticationDetailsProvider authenticationDetailsProvider(OCIClientProps clientProps) {
+    return SimpleAuthenticationDetailsProvider.builder()
+        .tenantId(clientProps.tenancyId())
+        .userId(clientProps.userId())
+        .fingerprint(clientProps.fingerprint())
+        .privateKeySupplier(getKey(clientProps.privateKey()))
+        .region(clientProps.region)
+        .build();
+  }
+
+  @Bean
+  public NotificationDataPlaneClient notificationDataPlaneClient(
+      AuthenticationDetailsProvider provider) {
+    return NotificationDataPlaneClient.builder().build(provider);
+  }
+
+
+  private Supplier<InputStream> getKey(String key) {
+    key = key.replaceAll(" ", "\n");
+    key = key.replace("-----BEGIN\nPRIVATE\nKEY-----", "-----BEGIN PRIVATE KEY-----");
+    key = key.replace("-----END\nPRIVATE\nKEY-----", "-----END PRIVATE KEY-----");
+
+    return new StringPrivateKeySupplier(key);
+  }
+}
