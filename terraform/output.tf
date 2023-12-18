b0cdbfa8b68c512a7de367255f35a31413a531aa
m.lepskaya <m.lepskaya@matilliom.com>
2023-12-18 16:56:59 +0100

    add back instance

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,5 @@
 output "function_test" {
-  value = "${oci_apigateway_deployment.manager_api_gw_deployment.endpoint}${oci_apigateway_deployment.manager_api_gw_deployment.specification[0].routes[0].path}"
+  value = "${oci_apigateway_deployment.function_api_gw_deployment.endpoint}${oci_apigateway_deployment.function_api_gw_deployment.specification[0].routes[0].path}"
 }
 output "list_all_objects_in_storage" {
   value = oci_objectstorage_preauthrequest.manage_preauth_request.full_path
