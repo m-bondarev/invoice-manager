e34b4600c5ff60b9677c67443acb4d8ce2f81613
Maxim Bondarev <m.bondarev@godeltech.com>
2023-12-19 14:07:36 +0100

    fix env variables

--- src/main/java/com/oci/invoicemanager/invoice/InvoiceService.java
+++ src/main/java/com/oci/invoicemanager/invoice/InvoiceService.java
@@ -1,5 +1,6 @@
 package com.oci.invoicemanager.invoice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oracle.bmc.queue.QueueClient;
 import com.oracle.bmc.queue.model.PutMessagesDetails;
 import com.oracle.bmc.queue.model.PutMessagesDetailsEntry;
@@ -8,6 +9,7 @@ import com.oracle.bmc.queue.requests.PutMessagesRequest;
 import com.oracle.bmc.queue.responses.PutMessagesResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,28 +22,35 @@ public class InvoiceService {
 
   private final QueueClient queueClient;
   private final NotificationService notificationService;
+  private final ObjectMapper objectMapper;
 
   @Value("${oci.queue.ocid}")
   private String queueId;
 
+  @SneakyThrows
   public PutMessagesResponse publish(
       final InvoiceDto invoiceDto
   ) {
     log.info("Publishing message {}", invoiceDto.getUserId());
 
+    final var putMessagesDetailsEntry = PutMessagesDetailsEntry.builder()
+        .content(objectMapper.writeValueAsString(invoiceDto))
+        .build();
+
+    final var putMessagesDetails = PutMessagesDetails.builder()
+        .messages(List.of(putMessagesDetailsEntry))
+        .build();
+
     final var putMessagesRequest = PutMessagesRequest.builder()
         .queueId(queueId)
-        .body$(PutMessagesDetails.builder().messages(List.of(PutMessagesDetailsEntry.builder()
-                .content(invoiceDto.toString())
-                .build()))
-            .build())
+        .body$(putMessagesDetails)
         .build();
 
 //    save to storage
 
 //    save to db
 
-//    notificationService.publishMessage(putMessagesRequest);
+    notificationService.publishMessage(putMessagesRequest);
 
     return queueClient.putMessages(putMessagesRequest);
   }
